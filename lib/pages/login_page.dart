bca250717f995b8dd4120cc77d816e79daf0f4ee
sarthak_nawaria <sarthak.nawaria.41@gmail.com>
2024-01-21 16:43:04 +0530

    first commit

---
+++ lib/pages/login_page.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+import '';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).colorScheme.background,
+      body: const Column(
+        children: [
+          //logo
+          Icon(Icons.message),
+          //welcome back message
+
+          //email textfield
+
+          //pw textfield
+
+          //login button
+
+          //register now
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/themes/light_mode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: ColorScheme.light(
+    primary: Colors.grey.shade500,
+    inversePrimary: Colors.grey.shade900,
+    secondary: Colors.grey.shade200,
+    background: Colors.grey.shade300,
+  ),
+);
