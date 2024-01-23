bc2bfb04658412a71631f76e0bcc0e00a85da93d
sarthak_nawaria <sarthak.nawaria.41@gmail.com>
2024-01-23 11:46:08 +0530

    feat : login inputs added

---
+++ lib/components/my_textfield.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class MyTextField extends StatelessWidget {
+  final bool obscureText;
+  final String hintText;
+
+  const MyTextField({super.key, required this.hintText , required this.obscureText});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 25.0),
+      child: TextField(
+        obscureText: false,
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+            borderSide:
+                BorderSide(color: Theme.of(context).colorScheme.tertiary),
+          ),
+          focusedBorder: OutlineInputBorder(
+              borderSide:
+                  BorderSide(color: Theme.of(context).colorScheme.primary)),
+          fillColor: Theme.of(context).colorScheme.secondary,
+          filled: true,
+          hintText: hintText,
+          hintStyle: TextStyle(color: Theme.of(context).colorScheme.primary),
+        ),
+      ),
+    );
+  }
+}
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,3 +1,4 @@
+import 'package:chat_app/components/my_textfield.dart';
 import 'package:flutter/material.dart';
 
 import '';
@@ -9,15 +10,44 @@ class LoginPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Theme.of(context).colorScheme.background,
-      body: const Column(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           //logo
-          Icon(Icons.message),
+          Icon(
+            Icons.message,
+            size: 60,
+            color: Theme.of(context).colorScheme.primary,
+          ),
+            const SizedBox(
+            height: 25,
+          ),
           //welcome back message
+          Text(
+            "Welcome back , you've been missed !",
+            style: TextStyle(
+              color: Theme.of(context).colorScheme.primary,
+              fontSize: 16,
+            ),
+          ),
 
+          const SizedBox(
+            height: 25,
+          ),
           //email textfield
-
+          MyTextField(
+            hintText: "Email",
+            obscureText:false,
+          ),
+         
+          const SizedBox(
+            height: 10,
+          ),
           //pw textfield
+          MyTextField(
+            hintText: "Password",
+            obscureText:true,
+          ),
 
           //login button
 
